5ca1a66a5f8e1524157ed3c3d0ef03a308213476
Kasp5953 <kasp5953@edu.easj.dk>
2018-02-20 13:43:07 +0100

    new stuff

---
+++ TCPEchoServer/TCPEchoServer/EchoService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPEchoServer {
+    public static class EchoService {
+        public static ThreadStart DoClient(TcpClient client) {
+            using (NetworkStream ns = client.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns)) {
+                sw.AutoFlush = true;
+                while (true) {
+                    //string readMessage = sr.ReadLine();
+                    string readMessage = "";
+                    try {
+                        readMessage = sr.ReadLine();
+                    } catch (IOException) {
+                        Console.WriteLine("Client connection closed.");
+                        return default;
+                    }
+                    string writeMessage = "";
+
+                    Console.WriteLine(readMessage);
+                    while (!String.IsNullOrWhiteSpace(readMessage)) {
+                        Console.WriteLine("Client: " + readMessage);
+                        writeMessage = readMessage.ToUpper();
+                        sw.WriteLine(writeMessage);
+                        try {
+                            readMessage = sr.ReadLine();
+                        } catch (IOException) {
+                            Console.WriteLine("Client connection closed.");
+                            return default;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- TCPEchoServer/TCPEchoServer/TCPEchoServer.cs
+++ TCPEchoServer/TCPEchoServer/TCPEchoServer.cs
@@ -15,6 +15,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TCPEchoServer {
@@ -29,70 +30,10 @@ namespace TCPEchoServer {
             
             while (true) {
                 TcpClient client = serverSocket.AcceptTcpClient();
-                Task.Run(() => DoClient(client));
-            }
-
-            /*
-            //TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-            //Socket connectionSocket = serverSocket.AcceptSocket();
-            //Console.WriteLine("Server activated");
-            Stream ns = connectionSocket.GetStream();
-            // Stream ns = new NetworkStream(connectionSocket);
-
-            StreamReader sr = new StreamReader(ns);
-            StreamWriter sw = new StreamWriter(ns);
-            sw.AutoFlush = true; // enable automatic flushing
-
-            string message = sr.ReadLine();
-            string answer = "";
-            while (message != null && message != "")
-            {
-
-                Console.WriteLine("Client: " + message);
-                answer = message.ToUpper();
-                sw.WriteLine(answer);
-                message = sr.ReadLine();
-
-            }
-            //ns.Close();
-            //connectionSocket.Close();
-            //serverSocket.Stop();
-            */
-        }
-
-        public static void DoClient(TcpClient client) {
-            using (NetworkStream ns = client.GetStream())
-            using (StreamReader sr = new StreamReader(ns))
-            using (StreamWriter sw = new StreamWriter(ns)) {
-                sw.AutoFlush = true;
-                while (true) {
-                    //string readMessage = sr.ReadLine();
-                    string readMessage = "";
-                    try {
-                        readMessage = sr.ReadLine();
-                    } catch (IOException) {
-                        Console.WriteLine("Client connection closed.");
-                        return;
-                    }
-                    string writeMessage = "";
-
-                    Console.WriteLine(readMessage);
-                    while(!String.IsNullOrWhiteSpace(readMessage)) {
-                        Console.WriteLine("Client: " + readMessage);
-                        writeMessage = readMessage.ToUpper();
-                        sw.WriteLine(writeMessage);
-                        try {
-                            readMessage = sr.ReadLine();
-                        } catch (IOException) {
-                            Console.WriteLine("Client connection closed.");
-                            return;
-                        }
-                    }
-                }
+                //Task.Run(() => EchoService.DoClient(client));
+                Thread t = new Thread(EchoService.DoClient(client));
+                t.Start();
             }
         }
     }
-
 }
-
-
